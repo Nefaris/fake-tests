7a833b45f00d547f7930b0e7b5286289e26d093e
Kamil Piękoś <kamcio190@gmail.com>
2023-07-07 23:29:45 +0200

    bump

---
+++ fake.test.js
@@ -0,0 +1,27 @@
+describe("Listing", () => {
+  it("should return list of 10 items", () => {});
+  it("should correctly change pages", () => {});
+  it("should display correct number of pages", () => {});
+});
+
+describe("Ad page", () => {
+  it("should display ad details", () => {});
+});
+
+describe("Chat", () => {
+  it("should send message", () => {});
+  it("should list messages", () => {});
+  it("should list all conversations", () => {});
+});
+
+describe("Creating new ads", () => {
+  it("should validate the form", () => {});
+  it("should display error messages for invalid inputs", () => {});
+  it("should display success message for valid inputs", () => {});
+});
+
+describe("Auth form", () => {
+  it("should validate the form", () => {});
+  it("should display error messages for invalid inputs", () => {});
+  it("should display success message for valid inputs", () => {});
+});
